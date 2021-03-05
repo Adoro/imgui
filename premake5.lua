d824d82777f901ff7b63061e9fdf5bb9929b960a
Tim van der Loo <timvanderloo@gmail.com>
2021-03-05 21:22:11 +0100

    Add premake and needed files

--- premake5.lua
+++ premake5.lua
@@ -7,16 +7,25 @@ project "ImGui"
 
 	files
 	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"*.h",
+		"*.cpp"
+	}
+
+	includedirs
+	{
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.GLEW}"
+	}
+
+	libdirs 
+    { 
+        "%{VendorDir.GLEW}/lib/Release/x64/"
+    }
+
+	links
+	{
+		"GLFW",
+		"glew32s.lib",
 	}
 
 	filter "system:windows"
